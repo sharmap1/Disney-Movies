eb5aba0e3ebd3f608727af8139a95b3adf5b536d
Prasamsha Sharma <prasamshari@gmail.com>
2021-04-27 18:24:37 -0700

    deleted unnecessary code from card

--- src/Cards.js
+++ src/Cards.js
@@ -1,38 +1,30 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Row,Col, Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import "./index.css";
 import { AiFillPlayCircle } from "react-icons/ai";
 
-
 function Cards(props) {
-   return (
+  return (
     <>
-    {/* <Container> */}
-      <div className="card1" 
-      style={{ backgroundColor: "thistle" , marginBottom:"20px"}}
+      <div
+        className="card1"
+        style={{ backgroundColor: "thistle", marginBottom: "20px" }}
       >
         <div className="overflow">
-      <img src={props.imgsrc} className="card-img-top" alt="cardmovie" />
-      <h5 className="card-title">{props.title}</h5>
-      <p className="card-heading">{props.moviename}</p>
-      <div style={{ marginBottom:"20px"}}>
-          <a href={props.link} target="blank">
-            <Button variant="light">
-              <AiFillPlayCircle style={{ color: "lightpink" }} />
-            </Button>
-          </a>
+          <img src={props.imgsrc} className="card-img-top" alt="cardmovie" />
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-heading">{props.moviename}</p>
+          <div style={{ marginBottom: "20px" }}>
+            <a href={props.link} target="blank">
+              <Button variant="light">
+                <AiFillPlayCircle style={{ color: "lightpink" }} />
+              </Button>
+            </a>
           </div>
         </div>
       </div>
-      {/* </Container> */}
     </>
   );
 }
 export default Cards;
-
-
-
-
-
-
